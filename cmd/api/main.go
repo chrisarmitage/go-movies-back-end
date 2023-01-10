fcfabe731dcb661ddaaba53e3d509712339f6ba0
Chris Armitage <dev@chrisarmitage.com>
2023-01-10 15:05:50 +0000

    New skeleton web server

---
+++ cmd/api/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+}
+
+func main() {
+	// set application config
+	var app application
+
+	// read flags from command line
+
+	// connect to database
+
+	app.Domain = "example.com"
+
+	// start web server
+	log.Println("Starting application on port", port)
+
+	http.HandleFunc("/", Hello)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
