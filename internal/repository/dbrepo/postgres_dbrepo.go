aca68efa478ef18fd52304573b175b378cbad2cf
Chris Armitage <dev@chrisarmitage.com>
2023-01-12 10:06:51 +0000

    Add PostgresDatabaseRepo::AllMovies

--- internal/repository/dbrepo/postgres_dbrepo.go
+++ internal/repository/dbrepo/postgres_dbrepo.go
@@ -2,15 +2,56 @@ package dbrepo
 
 import (
 	"backend/internal/models"
+	"context"
 	"database/sql"
+	"time"
 )
 
 type PostgresDbRepo struct {
 	Db *sql.DB
 }
 
+const dbTimeout = time.Second * 3
+
 func (r *PostgresDbRepo) AllMovies() ([]*models.Movie, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `
+		SELECT
+			id, title, release_date, runtime, mpaa_rating, description, coalesce(image, ''), created_at, updated_at
+		FROM 
+			movies
+		ORDER BY title
+	`
+
+	rows, err := r.Db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
 	var movies []*models.Movie
+
+	for rows.Next() {
+		var movie models.Movie
+		err := rows.Scan(
+			&movie.Id,
+			&movie.Title,
+			&movie.ReleaseDate,
+			&movie.RunTime,
+			&movie.MpaaRating,
+			&movie.Description,
+			&movie.Image,
+			&movie.CreatedAt,
+			&movie.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		movies = append(movies, &movie)
+	}
 
 	return movies, nil
 }
