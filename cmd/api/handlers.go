3ad54e51c5b54f2b7f16fb51ec0380a65a6a39e8
Chris Armitage <dev@chrisarmitage.com>
2023-01-13 09:06:50 +0000

    Add writeJson helper

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,74 +1,30 @@
 package main
 
 import (
-	"backend/internal/models"
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	var payload = struct {
-		Status string `json:"status"`
+		Status  string `json:"status"`
 		Message string `json:"message"`
 		Version string `json:"version"`
 	}{
-		Status: "active",
+		Status:  "active",
 		Message: "running",
 		Version: "1.0.0",
 	}
 
-	out, err := json.Marshal(payload)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.writeJson(w, http.StatusOK, payload)
 }
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
-	var movies []models.Movie
-
-	rd, _ := time.Parse("2006-01-02", "1986-03-07")
-
-	highlander := models.Movie{
-		Id: 1,
-		Title: "Highlander",
-		ReleaseDate: rd,
-		MpaaRating: "R",
-		RunTime: 116,
-		Description: "Desc of Highlander",
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	}
-
-	movies = append(movies, highlander)
-
-
-	rd, _ = time.Parse("2006-01-02", "1981-06-12")
-
-	rotla := models.Movie{
-		Id: 2,
-		Title: "Raiders of the Lost Ark",
-		ReleaseDate: rd,
-		MpaaRating: "PG-13",
-		RunTime: 115,
-		Description: "Desc of Raiders",
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	}
-
-	movies = append(movies, rotla)
-	
-	out, err := json.Marshal(movies)
+	movies, err := app.Db.AllMovies()
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.writeJson(w, http.StatusOK, movies)
 }
