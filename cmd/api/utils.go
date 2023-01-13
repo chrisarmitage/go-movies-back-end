13e08f7cf95efe6d44c377fbbe97479b4057d84d
Chris Armitage <dev@chrisarmitage.com>
2023-01-13 09:17:50 +0000

    Add errorJson helper

--- cmd/api/utils.go
+++ cmd/api/utils.go
@@ -59,3 +59,17 @@ func (app *application) readJson(w http.ResponseWriter, r *http.Request, data an
 
 	return nil
 }
+
+func (app *application) errorJson(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload JsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return app.writeJson(w, statusCode, payload)
+}
