5297c9090a355f43af0d7113796ecfc40656960a
Chris Armitage <dev@chrisarmitage.com>
2023-01-18 17:04:45 +0000

    JsonResponse field should be 'message'

--- cmd/api/utils.go
+++ cmd/api/utils.go
@@ -9,7 +9,7 @@ import (
 
 type JsonResponse struct {
 	Error   bool   `json:"error"`
-	Message string `json:"string"`
+	Message string `json:"message"`
 	Data    any    `json:"data,omitempty"`
 }
 
