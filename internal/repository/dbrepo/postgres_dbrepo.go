c761f033de9a7be06a2cac1b0da7e8c04a05810a
Chris Armitage <dev@chrisarmitage.com>
2023-01-18 16:27:33 +0000

    Add GetUserByEmail

--- internal/repository/dbrepo/postgres_dbrepo.go
+++ internal/repository/dbrepo/postgres_dbrepo.go
@@ -59,3 +59,35 @@ func (r *PostgresDbRepo) AllMovies() ([]*models.Movie, error) {
 
 	return movies, nil
 }
+
+func (r *PostgresDbRepo) GetUserByEmail(email string) (*models.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `
+		SELECT
+			id, email, first_name, last_name, password, created_at, updated_at
+		FROM
+			users
+		WHERE
+			email = $1
+	`
+
+	var user models.User
+
+	row := r.Db.QueryRowContext(ctx, query, email)
+
+	err := row.Scan(
+		&user.Id,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
